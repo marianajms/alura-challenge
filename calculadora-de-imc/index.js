d290cc6ac7d107ea018f5d7bf59dc1a3529fc6d4
Mariana Jager <mariana.macedosoares@hotmail.com>
2022-12-27 23:51:24 -0300

    Adicionei verificação de campos vazios

--- calculadora-de-imc/index.js
+++ calculadora-de-imc/index.js
@@ -12,11 +12,28 @@ BfecharModal.addEventListener('click',FecharModal);
 
 
 var imc;
+var campoVazio=false;
+
+function VerificarCamposVazios(){
+    if(inputAltura.value==''&&inputPeso.value==''){
+        campoVazio=true;
+        return 'Adicione seu peso e altura para calcular seu IMC';
+    }else if (inputAltura.value==''){
+        campoVazio=true;
+        return 'Adicione sua altura para calcular seu IMC';
+    }else if(inputPeso.value==''){
+        campoVazio=true;
+        return 'Adicione seu peso para calcular seu IMC';
+    } else campoVazio=false;
+    
+}
 
 function CalcularIMC(altura,peso){
     let imc = peso/(altura*altura);
     return imc;
 }
+
+
 
 function VerificarIMC(imc){
     if(imc<18.5){
@@ -34,12 +51,18 @@ function VerificarIMC(imc){
 
 function ClicouBotao(event){
     event.preventDefault();
+    VerificarCamposVazios();
 
-    let alturaFormatada = Number(inputAltura.value)/100;
-    imc = CalcularIMC(alturaFormatada,inputPeso.value);
-    resultadoTexto.innerText = `Seu imc é ${imc.toFixed(2)} e está ${VerificarIMC(imc)}`
-    screenResultado.classList.remove('hide');
- 
+    if(campoVazio){
+        resultadoTexto.innerText = VerificarCamposVazios();
+        screenResultado.classList.remove('hide');
+    }else{
+        let alturaFormatada = Number(inputAltura.value)/100;
+        imc = CalcularIMC(alturaFormatada,inputPeso.value);
+        resultadoTexto.innerText = `Seu imc é ${imc.toFixed(2)} e está ${VerificarIMC(imc)}`;
+        screenResultado.classList.remove('hide');
+    }
+
 }
 
 function FecharModal(){
